0d0863f4bfae847a6fb9639da8c8193214204492
Simon Schneider <simon.sch@outlook.com>
2014-10-11 15:17:05 +0200

    Some Test 2

---
+++ public/doc/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Broking Club Documentation</title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Broking Club Documentation</h1>
+        </div>
+    </body>
+
+</html>
