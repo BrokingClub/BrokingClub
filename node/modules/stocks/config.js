1cf59c058cccb252cf9098f26af70df267bc252c
Marc <marc.vornetran@gmail.com>
2014-11-25 10:33:54 +0100

    Ignore and delete start.sh, use env variables

--- node/modules/stocks/config.js
+++ node/modules/stocks/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	sql: {
-		user: 'root',
-		password: 'A8XZrP^atr7$2Pd',
-		database: 'brokingclub'
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE
 	},
 	fetchDelaySec: 60,
     deleteOldStocksDelayHours: 1
