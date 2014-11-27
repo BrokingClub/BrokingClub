a437ad97eae7fb3b36d1443c298ce7c55987bf5d
SCHEMPIL <PhilippSchemel@live.com>
2014-11-27 10:00:20 +0100

    user info show.blade.php

--- laravel/app/views/pages/game/user/edit.blade.php
+++ laravel/app/views/pages/game/user/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.game')
 
 @section('content')
+
     {{ Fickle::openTabbedPanel(12,array('profile' => 'Personal Info', 'password' => 'Change Password',
         'club' => 'My Broking Club', 'delete' => 'Close Account')) }}
         {{ Fickle::openTabContent('profile') }}
---
+++ laravel/app/views/pages/game/user/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.game')
+
+@section('content')
+
+    {{ Fickle::openPanel('User information', 12) }}
+        user info
+    {{Fickle::closePanel()}}
+
+
+
+
+
+@endsection
