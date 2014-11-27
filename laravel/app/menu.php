f5b661dda7d98df4d09328c259f6375e6d897190
SCHEMPIL <PhilippSchemel@live.com>
2014-11-27 14:19:42 +0100

    index.blade.php

--- laravel/app/menu.php
+++ laravel/app/menu.php
@@ -4,6 +4,8 @@ Menu::make('MainMenu', function($menu){
     $menu->add('Home', '')->data('icon', 'dashboard');
     $menu->add('Profile', 'profile')->data('icon', 'user');
     $menu->add('Stocks', 'stocks')->data('icon', 'line-chart');
+    $menu->add('Clubs', 'clubs')->data('icon', 'mortar-board');
+    $menu->add('Ranking', 'players')->data('icon', 'users');
 
 });
 
