9292e6638a6abbfbe26213bfd1089aa5b8d71050
Marc <marc.vornetran@gmail.com>
2014-12-01 11:10:28 +0100

    Stock crawler timer auslagern

--- node/modules/stocks.js
+++ node/modules/stocks.js
@@ -4,31 +4,34 @@ var config = require('./config');
 var no = require('app/no');
 var _ = require('lodash');
 var cache = {};
+var symbols;
 
 sql.query('SELECT id, symbol FROM stocks', function(err, result){
     if(no(err)){
-        startFetchInterval(result);
+        symbols = result;
+        
+        startFetchInterval();
     }
 });
 
 startDeleteOldStocksInterval();
 
-function startFetchInterval(symbols){
+function startFetchInterval(){
 	var callback = function(){
         var start = Date.now();
         
-		fetchStocks(symbols);
+		fetchStocks(_.clone(symbols));
         console.log('Fetched stocks in ' + (Date.now() - start) + ' ms');
 	};
 	
 	callback();
-	setInterval(callback, config.fetchDelaySec * 1000);
+	setInterval(callback, 10000);//config.fetchDelaySec * 1000);
 }
 
 function fetchStocks(symbols){
 	var yql = buildYqlQuery(symbols);
 	var apiUrl = 'https://query.yahooapis.com/v1/public/yql?q=' + yql + '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
-	
+    
 	request.get(apiUrl, function(err, res, body){
 		if(no(err)){
 			body = JSON.parse(body);
@@ -40,17 +43,17 @@ function fetchStocks(symbols){
                     symbols[i].quote = quotes[i].LastTradePriceOnly;
                 }
                 
-                symbols = checkForUnchangedQuotes(symbols);
+                var changedSymbols = checkForUnchangedQuotes(symbols);
+                cache.symbols = symbols;
                 
-                if(symbols.length){
-                    cache.symbols = symbols;
-                    
+                if(changedSymbols.length){
+                    console.log('Saving ' + changedSymbols.length + ' of ' + symbols.length + ' quotes');
                     saveQuotes(symbols);
                 }else{
                     console.log('Unchanged quotes');   
                 }
             }else{
-                throw new Error('Invalid JSON response from Yahoo API: ' + body);
+                throw new Error('Invalid JSON response from Yahoo API');
             }
 		}
 	});
@@ -97,12 +100,12 @@ function deleteOldStocks(){
 function checkForUnchangedQuotes(symbols){
     if(cache.symbols){
         cache.symbols.forEach(function(cacheSymbol){
-            var symbol = _.find(symbols, cacheSymbol.id);
+            var symbol = _.find(symbols, { id: cacheSymbol.id });
             
             if(symbol){
                 if(symbol.quote === cacheSymbol.quote){
                     console.log('Unchanged quote found for symbol ' + symbol.symbol);
-                    _.remove(symbols, symbol.id);   
+                    _.remove(symbols, { id: symbol.id });   
                 }
             }
         });
