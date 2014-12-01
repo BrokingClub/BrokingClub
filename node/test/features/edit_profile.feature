6cdf4446dcd9af6bbadeb55f5e876bd0201979bc
Marc <marc.vornetran@gmail.com>
2014-12-01 20:09:29 +0100

    #bc-28 work 1h

--- node/test/features/edit_profile.feature
+++ node/test/features/edit_profile.feature
@@ -1,18 +1,18 @@
 Feature: change the personal information of my account
-	As a logged in user
-	I want to edit my profile
-	So I can correct my name or change my username
+  As a logged in user
+  I want to edit my profile
+  So I can correct my name or change my username
 	
-    Background:
-        Given I am logged in as a test user
-        And I am on the profile page
+  Background:
+    Given I am logged in as a test user
+    Given I am on the profile page
     
-	Scenario: Enter valid first name and last name and an available username
-		And I enter the following text in first name: "first_name"
-		And I enter the following text in last name: "last_name"
-		Then I should see "Profile has been updated"
+    Scenario: Enter valid first name and last name and an available username
+      When I enter the following text in first name: "first_name"
+      When I enter the following text in last name: "last_name"
+      Then I should see "Profile has been updated"
 		
-	Scenario: I do not enter a valid first name or/and last name or/and username into the firstname field, the lastname field or the username field
-		And I enter the following text in first name: "%/&_not_a_valid_firstname:)" 
-		And I enter the following text in last name: "&%not_a_valid_lastname(/"
-		Then I should see "Non valid input!"
+    Scenario: I do not enter a valid first name or/and last name or/and username into the firstname field, the lastname field or the username field
+      When I enter the following text in first name: "%/&_not_a_valid_firstname:)" 
+      When I enter the following text in last name: "&%not_a_valid_lastname(/"
+      Then I should see "Non valid input!"
--- node/test/features/step_definitions/edit_profile.js
+++ node/test/features/step_definitions/edit_profile.js
@@ -2,31 +2,36 @@ var no = require('app/no');
 
 module.exports = function(){
     this.World = require('../support/world.js').World;
-
+    
     /* Background */
     this.Given('I am logged in as a test user', function(callback){
+        var browser = this.browser;
+        
         this.visit('http://broking.club/login', function(err){
             if(no(err)){
-                this.browser
+                callback.pending();
+                browser
                     .fill('email', 'test@broking.club')
-                    .fill('password', 'test')
-                    .pressButton('button', callback);
+                    .fill('password', 'test');
+                    /*.pressButton('button', function(err){
+                        callback.pending();
+                    });*/
             }else{
                 callback.fail(err);
             }
         });
     });
     
-    this.And('I am on the profile page', function(callback){
+    this.Given('I am on the profile page', function(callback){
         callback.pending();
     });
     /* Background */
     /* Scenario */
-    this.And('I enter the following text in first name: "first_name"', function(firstName, callback){
+    this.When('I enter the following text in first name: "first_name"', function(firstName, callback){
         callback.pending();
     });
     
-    this.And('I enter the following text in last name: "last_name"', function(lastName, callback){
+    this.When('I enter the following text in last name: "last_name"', function(lastName, callback){
         callback.pending();
     });
     
@@ -34,11 +39,11 @@ module.exports = function(){
         callback.pending();
     });
     /* Scenario */
-    this.And('I enter the following text in first name: "%/&_not_a_valid_firstname:)"', function(callback){
+    this.When('I enter the following text in first name: "%/&_not_a_valid_firstname:)"', function(callback){
         callback.pending();
     });
     
-    this.And('I enter the following text in last name: "&%not_a_valid_lastname(/"', function(callback){
+    this.When('I enter the following text in last name: "&%not_a_valid_lastname(/"', function(callback){
         callback.pending();
     });
     
