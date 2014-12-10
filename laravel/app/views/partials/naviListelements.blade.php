182cef066882adb48241039c2d906fe5e27d58ad
triggerdesign <trigger3@hotmail.de>
2014-12-10 22:26:27 +0100

    Removed small screen navi error

--- laravel/app/views/partials/naviListelements.blade.php
+++ laravel/app/views/partials/naviListelements.blade.php
@@ -1,6 +1,7 @@
 @foreach($items as $item)
   <li @if($item->hasChildren()) class="dropdown" @endif>
-      <a href="{{ $item->url() }}"><i class="fa fa-{{ $item->icon }}"></i> {{ $item->title }} </a>
+      <a href="{{ $item->url() }}"><i class="fa fa-{{ $item->icon }}"></i>
+      <span>{{ $item->title }}</span></a>
       @if($item->hasChildren())
         <ul class="dropdown-menu">
               @include('custom-menu-items', array('items' => $item->children()))
