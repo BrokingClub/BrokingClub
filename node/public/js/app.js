9852c4ee5298157d7f2d9293e7763afe7e39bc35
Marc <marc.vornetran@gmail.com>
2015-04-16 14:13:59 +0200

    New node landing page

--- node/public/js/app.js
+++ node/public/js/app.js
@@ -4,12 +4,20 @@
 	var app = angular.module('app', ['ngRoute']);
 	
 	app.config(function($routeProvider){
-		$routeProvider.
-			when('/feature/:feature', {
-				templateUrl: 'views/feature.html',
-				controller: 'feature'
-			}).
-			otherwise({
+		$routeProvider
+            .when('/', {
+                templateUrl: 'views/dashboard.html',
+                controller: 'DashboardController'
+            })
+            .when('/cucumber/:feature', {
+                templateUrl: 'views/cucumber.html',
+                controller: 'CucumberController'
+            })
+            .when('/linesofcode', {
+                templateUrl: 'views/lines-of-code.html',
+                controller: 'LinesOfCodeController'
+            })
+            .otherwise({
 				redirectTo: '/'
 			});
 	});
@@ -17,8 +25,20 @@
 	app.factory('feature', function(){
 		return { name: '' };
 	});
-	
-	app.controller('controller', function($scope, $http, feature){
+
+    app.run(function($rootScope){
+        $rootScope.currentPage = 'Dashboard';
+    });
+
+    app.controller('DashboardController', function($scope){
+
+    });
+
+    app.controller('LinesOfCodeController', function($scope){
+
+    });
+
+	app.controller('CucumberController', function($routeParams, $scope, $http, $interval, feature){
 		$http.get('/api/cucumber/features').success(function(features){
 			$scope.features = features;
 		});
@@ -26,46 +46,44 @@
 		$scope.isCurrent = function(featureName){
 			return featureName === feature.name ? 'active' : '';
 		};
-	});
-	
-	app.controller('feature', function($routeParams, $scope, $http, $interval, feature){
-		feature.name = $routeParams.feature;
+
+        feature.name = $routeParams.feature;
         $scope.output = '';
         $scope.testing = true;
-        
+
         $http.get('/api/cucumber/features/' + feature.name).success(function(data){
             $scope.featureText = data.feature;
             $scope.source = data.source;
-            
+
             setTimeout(function(){
                 $('div.gherkin, div.javascript').each(function(i, block){
-                    hljs.highlightBlock(block); 
+                    hljs.highlightBlock(block);
                 });
             }, 1);
-        });  
-        
+        });
+
         var socket = io.connect();
-        
+
         socket.emit('feature', feature.name);
         socket.on('data', function(data){
             $scope.$apply(function(){
-                $scope.output += data; 
+                $scope.output += data;
             });
         });
         socket.on('end', function(){
-            $scope.testing = false; 
+            $scope.testing = false;
         });
-        
+
         $scope.progressText = function(){
             return $scope.testing ? 'Crunching the latest data, just for you. Hang tight...' : '';
         };
-        
+
         $scope.progressClass = function(){
             return $scope.testing ? 'progress-bar-striped active' : 'progress-bar-success';
         };
-        
+
         $interval(function(){
-            $scope.cursor = !$scope.cursor;   
+            $scope.cursor = !$scope.cursor;
         }, 500);
 	});
 }());
