64074a4f39236bf1ec7b876b161dfa8935ccc2a0
Philipp Schemel <PhilippSchemel@live.com>
2015-04-16 16:10:43 +0200

    need another beer.

--- laravel/app/views/pages/admin/index.blade.php
+++ laravel/app/views/pages/admin/index.blade.php
@@ -3,7 +3,22 @@
 @section('content')
 
     {{ Fickle::openPanel('Administration area', 12) }}
-        Administrate your shit
+        {{Fickle::openTable()}}
+            <tr>
+            <td>UserId</td>
+            <td>Username</td>
+            <td>E-Mail</td>
+            <td>Role</td>
+            </tr>
+            @foreach($users AS $user)
+            <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->role }}</td>
+            </tr>
+            @endforeach
+        {{Fickle::closeTable()}}
     {{ Fickle::closePanel() }}
 
 @endsection
