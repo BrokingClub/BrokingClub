b302bc740b6c97866e5d23f13134c9bb12c75c11
Philipp Schemel <PhilippSchemel@live.com>
2015-04-16 15:33:55 +0200

    need another beer

--- laravel/app/views/pages/admin/index.blade.php
+++ laravel/app/views/pages/admin/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-    <h2>Administrate</h2>
+    {{ Fickle::openPanel('Administration area', 12) }}
+        @if(Player::auth() && Player::auth()->user->role == 'admin')
+            Administrate your shit
+        @else
+            <h2>Authentification failed</h2>
+        @endif
+    {{ Fickle::closePanel() }}
 
 @endsection
